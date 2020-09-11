ee3a07a36c3e30cc8de87d824b919c6a6decf1ef
Max Moskalchuk <gmxiiim@gmail.com>
2020-09-11 12:58:59 +0300

    Add Git case sensitivity

---
+++ src/components/bookmarks/mock-bookmarks.ts
@@ -0,0 +1,13 @@
+const mockBookmarks = [
+  { id: 1, name: 'Hacker News', url: 'https://news.ycombinator.com/' },
+  { id: 2, name: 'Discord', url: 'https://discord.com/' },
+  { id: 3, name: 'GitHub', url: 'https://github.com/' },
+  { id: 4, name: 'Hacker News', url: 'https://news.ycombinator.com/' },
+  { id: 5, name: 'Discord', url: 'https://discord.com/' },
+  { id: 6, name: 'GitHub', url: 'https://github.com/' },
+  { id: 7, name: 'Hacker News', url: 'https://news.ycombinator.com/' },
+  { id: 8, name: 'Discord', url: 'https://discord.com/' },
+  { id: 9, name: 'GitHub', url: 'https://github.com/' },
+];
+
+export { mockBookmarks };
