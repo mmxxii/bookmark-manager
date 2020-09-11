77179fd53f88140ae52075305831d1065f9ce75c
Max Moskalchuk <gmxiiim@gmail.com>
2020-09-11 11:52:25 +0300

    Create Sidebar component, refactor styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 
 import styles from './App.module.scss';
 
@@ -8,31 +8,21 @@ import Brand from './components/Brand';
 import Auth from './components/Auth';
 import BookmarkTree from './components/BookmarkTree';
 import Bookmarks from './components/Bookmarks';
+import Sidebar from './components/Sidebar';
 
 // const BookmarkTree = React.lazy(() => import('./components/BookmarkTree'));
 // const Bookmarks = React.lazy(() => import('./components/Bookmarks'));
-
-const sidebarVariants = {
-  start: { width: '45%' },
-  expanded: { width: '50%', transition: { type: 'spring' } },
-  narrow: { width: 400, transition: { type: 'spring' } },
-};
 
 const App: FC = () => {
   const { user } = useAuth();
 
   return (
     <div className={styles.container}>
-      <motion.div
-        initial="start"
-        animate={user ? 'narrow' : 'expanded'}
-        variants={sidebarVariants}
-        className={styles.sidebar}
-      >
+      <Sidebar>
         <AnimatePresence exitBeforeEnter>
           {user ? <BookmarkTree key="bookmark-tree" /> : <Brand key="brand" />}
         </AnimatePresence>
-      </motion.div>
+      </Sidebar>
       <div className={styles.content}>
         <AnimatePresence exitBeforeEnter>
           {user ? <Bookmarks key="bookmarks" /> : <Auth key="auth" />}
