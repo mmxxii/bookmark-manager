192fe1e16f7d8ae9fc332d32b19b618cdf9abdec
Max Moskalchuk <gmxiiim@gmail.com>
2020-09-11 16:35:55 +0300

    Create AuthIcon component

--- src/components/auth/Auth/Auth.tsx
+++ src/components/auth/Auth/Auth.tsx
@@ -1,55 +1,18 @@
 import React, { FC } from 'react';
-import { AnimateSharedLayout, motion } from 'framer-motion';
+import { motion, AnimateSharedLayout } from 'framer-motion';
 
 import styles from './Auth.module.scss';
 
-import {
-  exitVariants,
-  svgCircleVariants,
-  svgPathVariants,
-} from '../../../lib/motion-variants';
-import { Login, Signup } from '..';
+import { exitVariants } from '../../../lib/motion-variants';
+import { AuthIcon, Login, Signup } from '..';
 import { Tabs, Tab } from '../../layout';
-
-const iconWrapperVariants = {
-  initial: { scale: 0.5 },
-  animate: { scale: 1, transition: { type: 'spring' } },
-};
 
 const Authentication: FC = () => {
   return (
     <motion.div animate="animate" exit="exit" variants={exitVariants}>
       <AnimateSharedLayout>
         <div className={styles.container}>
-          <motion.div
-            layout
-            initial="initial"
-            animate="animate"
-            variants={iconWrapperVariants}
-            className={styles.iconWrapper}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              className={styles.icon}
-            >
-              <motion.path
-                initial="initial"
-                animate="animate"
-                variants={svgPathVariants}
-                d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"
-              ></motion.path>
-              <motion.circle
-                initial="initial"
-                animate="animate"
-                variants={svgCircleVariants}
-                cx="12"
-                cy="7"
-                r="4"
-              ></motion.circle>
-            </svg>
-          </motion.div>
+          <AuthIcon />
           <Tabs>
             <Tab label="Log In">
               <Login />
--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,3 +1,4 @@
 export { default as Auth } from './Auth/Auth';
+export { default as AuthIcon } from './AuthIcon/AuthIcon';
 export { default as Login } from './Login/Login';
 export { default as Signup } from './Signup/Signup';
