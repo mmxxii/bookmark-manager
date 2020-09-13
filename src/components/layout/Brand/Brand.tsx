d3209f7afabae1a83782b83b8961dd86e0441f75
Max Moskalchuk <gmxiiim@gmail.com>
2020-09-13 20:59:44 +0300

    Remove SVG icons

--- src/components/layout/Brand/Brand.tsx
+++ src/components/layout/Brand/Brand.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 import styles from './Brand.module.scss';
 
-import { exitVariants, svgPathVariants } from '../../../lib/motion-variants';
+import { exitVariants } from '../../../lib/motion-variants';
 
 const Brand: FC = () => {
   return (
@@ -15,19 +15,6 @@ const Brand: FC = () => {
       className={styles.container}
     >
       <div className={styles.wrapper}>
-        <svg
-          className={styles.bookmark}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-        >
-          <motion.path
-            initial="initial"
-            animate="animate"
-            variants={svgPathVariants}
-            d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"
-          ></motion.path>
-        </svg>
         <h1 className={styles.heading}>
           Bookmark
           <br />
--- src/lib/motion-variants.ts
+++ src/lib/motion-variants.ts
@@ -1,21 +1,3 @@
-// SVG
-export const svgPathVariants = {
-  initial: { pathLength: 0, opacity: 0 },
-  animate: {
-    pathLength: 1,
-    opacity: 1,
-    transition: { type: 'tween', duration: 1, delay: 0.5, ease: 'easeInOut' },
-  },
-};
-
-export const svgCircleVariants = {
-  initial: { opacity: 0 },
-  animate: {
-    opacity: 1,
-    transition: { type: 'tween', duration: 1, delay: 0.5, ease: 'easeInOut' },
-  },
-};
-
 export const exitVariants = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
